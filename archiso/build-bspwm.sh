1cfc1a922e0748cfa99fedb0d712994924248c92
PeterDauwe <bobo5290461@gmail.com>
2020-03-27 12:40:22 +0100

    up

--- archiso/build-bspwm.sh
+++ archiso/build-bspwm.sh
@@ -3,7 +3,7 @@
 set -e -u
 
 iso_name=arcobobo
-iso_label="arcobobo"
+iso_label="arcobobo-bspwm-y$(date +%y).m$(date +%m).d$(date +%d)"
 iso_publisher="ArcoLinux <http://www.arcolinux.info>"
 iso_application="ArcoLinux Live/Rescue CD"
 iso_version="y$(date +%y).m$(date +%m).d$(date +%d)"
